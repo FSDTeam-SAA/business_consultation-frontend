ee22f3845bd12c28f4b37973dfe0a7dafb55518f
badhon252 <khalidhossainbadhon@gmail.com>
2025-04-15 11:33:00 +0600

    refactor

--- src/app/account/layout.tsx
+++ src/app/account/layout.tsx
@@ -1,9 +1,27 @@
-import React from "react";
+import "@/app/globals.css";
+import type { Metadata } from "next";
+import Navbar from "@/components/navar";
+import Footer from "@/components/footer";
+import NewsletterSection from "@/components/newsletter-section";
 
-export default function layout({
+export const metadata: Metadata = {
+  title: "Account",
+  description: "Account",
+};
+
+export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <div>{children}</div>;
+  return (
+    <html lang="en">
+      <body className={``}>
+        <Navbar />
+        {children}
+        <NewsletterSection />
+        <Footer />
+      </body>
+    </html>
+  );
 }
